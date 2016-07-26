f6780eb6a97b07bcecd66dd17b3d5871528f55c7
nik <npejic@gmail.com>
2016-07-26 11:39:29 +0200

    gotovo

---
+++ TPT-part20/TPT-part20/EmployeeDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TPT_part20
+{
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+/*        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ContractEmployee>().ToTable("ContractEmployees");
+            modelBuilder.Entity<PermanentEmployee>().ToTable("PermanentEmployees");
+
+            base.OnModelCreating(modelBuilder);
+        } 
+ ovo moze da stoji umesto Table atributa u klasama Employee, PermanentEmployee i ContractEmployee*/
+
+    }
+}
